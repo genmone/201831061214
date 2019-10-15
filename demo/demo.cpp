446d90cab60940e765f73e19e51f3b7d51badde7
genmone <54836819+genmone@users.noreply.github.com>
2019-10-15 20:31:08 +0800

    Add files via upload

---
+++ demo/demo.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+
+using namespace std;
+
+
+struct zifu
+{
+	char word;
+	int number;
+	struct zifu* next;
+};
+
+class way_to_char
+{
+public:
+	int set()
+	{
+		use = new struct zifu;
+		use->next = NULL;
+		if (use == NULL)
+		{
+			cout << "分配内存失败" << endl;
+		}
+		cout << use << endl;
+		return 0;
+	}
+	int char_counter(char b)
+	{
+		if (use == NULL)
+		{
+			return 0;
+		}
+		struct zifu* p;
+		for (p = use->next; p != NULL; p = p->next)
+		{
+			if (p->word == b)
+			{
+				p->number++;
+				char_number++;
+				return 0;
+			}
+		}
+		p = new struct zifu;
+		p->word = b;
+		p->number = 1;
+		p->next = use->next;
+		use->next = p;
+		char_number++;
+		return 0;
+	}
+
+	int show()
+	{
+		if (use == NULL)
+			return 0;
+		struct zifu* p;
+		char stead;
+		cout << "总的字符数为" << char_number  << endl;
+		for (p = use->next; p != NULL; p = p->next)
+		{
+			if (p->word >= 65 && p->word <= 90)
+				stead = p->word + 32;
+			else
+				stead = p->word;
+			cout << "字符为" << stead << "出现次数为" << p->number << endl;
+		}
+		return 0;
+	}
+	int fun(char b[])
+	{
+		int a;
+		set();
+		for (a = 0; b[a] != '\0'; a++)
+		{
+			char_counter(b[a]);
+		}
+		return 0;
+	}
+private:
+	struct zifu* use;
+	int char_number = 0;
+};
+
+int main()
+{
+	ifstream File("text.txt", ios::in);
+	if (!File)
+	{
+		File.close();
+		cout << "ERROR" << endl;
+		return -1;
+	}
+	char x;
+	//cout << "输入字符串" << endl;
+	char usef[100];
+	int i = 0;
+	way_to_char c;
+	File.getline(usef, 100);
+	cout << usef << endl;
+	cout << "进行字符串处理" << endl;
+	c.fun(usef);
+	cout << "结果展示" << endl;
+	c.show();
+	File.close();
+	return 0;
+}
